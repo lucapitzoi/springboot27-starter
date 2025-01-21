2d7804c4163b1e4ec36747085277bc26191416d4
lucapitzoi <luca.pitzoi@sindria.org>
2025-01-21 19:51:28 +0100

    fix

--- src/src/test/java/org/sindria/xpipe/lib/springboot27-starter/SpringBootStarterApplicationTests.java
+++ src/src/test/java/org/sindria/xpipe/lib/springboot27-starter/SpringBootStarterApplicationTests.java
@@ -1,10 +1,10 @@
-package org.sindria.xpipe.core.policies;
+package org.sindria.xpipe.lib.springboot27-starter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class XPipePoliciesApplicationTests {
+class SpringBootStarterApplicationTests {
 
 // 	@Test
 // 	void contextLoads() {
